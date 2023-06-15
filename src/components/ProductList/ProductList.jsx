bfbc4b0798d5f966aa34e734a62c4380b6c987fe
Adarsh Dalvi <adarshdalvi197@.com>
2023-06-16 00:28:52 +0530

    fixing build

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductCard from '../ProductCard/ProductCard'
 import { useEffect, useState } from 'react'
 import { getAllProducts } from '../../utils/api'
 import Skeleton from '../Skeleton/Skeleton'
-import SomethingWentWrong from '../SomethingWentWrong/SomethingWentWrong'
+import SomethingWentWrong from '../SomethingWentWrong/SomethingWrong'
 
 export default function ProductList({filteredData}) {
 
