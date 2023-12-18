afe18620b6e59694d2d2c9dbddbfc95d25ffca88
Adarsh Dalvi <adarshdalvi197@.com>
2023-12-18 15:36:58 +0530

    changed category id in featuredlatest page

--- src/components/Featured/Featured_Latest.jsx
+++ src/components/Featured/Featured_Latest.jsx
@@ -4,22 +4,22 @@ import './Featured_Latest.scss'
 import sampleSize from 'lodash/sampleSize'
 
 
-export default function Featured_Latest({data, heading}) {
+export default function Featured_Latest({ data, heading }) {
 
   const randomArray = sampleSize(data, 5)
   return (
     <div className='featured'>
       <div className='featured-heading'>
         <h1>{heading}</h1>
-        <NavLink to='/products/6'>
+        <NavLink to='/products/3'>
           <p>view all</p>
         </NavLink>
       </div>
       <div className='featured-grid'>
         {
-          randomArray?.map(product=>{
+          randomArray?.map(product => {
             return (
-              <ProductCard 
+              <ProductCard
                 key={product?.id}
                 data={product}
               />
